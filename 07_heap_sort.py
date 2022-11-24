88356314c5dc4c9326283294a742fbeffd58d8ec
Eugenio <lugezz@gmail.com>
2022-11-24 08:37:03 -0300

    Merge sort

---
+++ 07_heap_sort.py
@@ -0,0 +1,8 @@
+"""
+Heapsort is a comparison-based sorting technique based on a Binary Heap data structure. It is
+similar to selection sort where we first find the maximum element and place the maximum element
+at the end. We repeat the same process for the remaining element.
+
+Time complexity: O(n log n).
+Space complexity of O(n).
+"""
